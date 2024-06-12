c4615b9e259622df4bbccd217348ea4c0b7756dd
Sunish Sheth <sunishsheth2009@gmail.com>
2024-06-12 12:19:47 -0700

    Resolving based on comments
    Signed-off-by: Sunish Sheth <sunishsheth2009@gmail.com>

--- examples/langchain/chain_as_code_driver.py
+++ examples/langchain/chain_as_code_driver.py
@@ -1,7 +1,9 @@
+# This is an example for logging a Langchain model as code using the
+# mlflow.langchain.log_model API by passing in the corresponding chain_as_code.py path,
+# which avoids serializing the model but rather stores the chain code as an artifact.
+# The model is set using mlflow.models.set_model in the chain code is the chain object
+# that will be loaded back using mlflow.langchain.load_model, which can be used for inference.
 import mlflow
-
-# Use the Unity Catalog model registry
-mlflow.set_registry_uri("databricks-uc")
 
 input_example = {
     "messages": [
@@ -17,7 +19,7 @@ chain_notebook_path = "chain_as_code.py"
 
 print(f"Chain notebook path: {chain_notebook_path}")
 
-print("Logging model model as code using Langchain log model API")
+print("Logging model as code using Langchain log model API")
 with mlflow.start_run():
     logged_chain_info = mlflow.langchain.log_model(
         lc_model=chain_notebook_path,
